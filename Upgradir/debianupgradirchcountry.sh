e59d241258a86549ad371e05dddd32d92367de8a
nu11secur1ty <venvaropt@gmail.com>
2019-10-24 14:44:55 +0300

    Update debianupgradirchcountry.sh

--- Upgradir/debianupgradirchcountry.sh
+++ Upgradir/debianupgradirchcountry.sh
@@ -7,7 +7,11 @@ os.system("apt dist-upgrade -y");
 os.system("apt autoremove -y");
 os.system("apt --fix-broken install -y");
 os.system("cp /etc/apt/sources.list /etc/apt/sources.list_backup");
-os.system("sed -i 's/ftp.bg/ftp.gr/' /etc/apt/sources.list");
+ echo "Type your country for exam: br, eu, gr"
+  read=country
+  echo "Type your country which you want to use for exam: br, eu, gr"
+  read=usage
+os.system("sed -i 's/$country/$usage/' /etc/apt/sources.list");
 os.system("apt update -y");
 os.system("apt upgrade -y");
 os.system("apt dist-upgrade -y");
