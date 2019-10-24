1f274461cbb9ad2eae491adfe5d4e839bbbfb282
nu11secur1ty <venvaropt@gmail.com>
2019-10-24 14:52:49 +0300

    Update debianupgradirchcountry.sh

--- Upgradir/debianupgradirchcountry.sh
+++ Upgradir/debianupgradirchcountry.sh
@@ -7,10 +7,10 @@ os.system("apt dist-upgrade -y");
 os.system("apt autoremove -y");
 os.system("apt --fix-broken install -y");
 os.system("cp /etc/apt/sources.list /etc/apt/sources.list_backup");
- echo "Type your country for exam: br, eu, gr"
-  read country
-  echo "Type your country which you want to use for exam: br, eu, gr"
-  read usage
+ os.system(echo "Type your country for exam: br, eu, gr");
+  os.system(read country);
+  os.system(echo "Type your country which you want to use for exam: br, eu, gr")
+  os.system(read usage)
 os.system("sed -i 's/$country/$usage/' /etc/apt/sources.list");
 os.system("apt update -y");
 os.system("apt upgrade -y");
