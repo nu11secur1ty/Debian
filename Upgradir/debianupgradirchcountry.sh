c7823d73097413ccf56ace74344db7b76cb34afd
nu11secur1ty <venvaropt@gmail.com>
2019-10-24 14:58:45 +0300

    Update debianupgradirchcountry.sh

--- Upgradir/debianupgradirchcountry.sh
+++ Upgradir/debianupgradirchcountry.sh
@@ -7,6 +7,7 @@ os.system("apt dist-upgrade -y");
 os.system("apt autoremove -y");
 os.system("apt --fix-broken install -y");
 os.system("cp /etc/apt/sources.list /etc/apt/sources.list_backup");
+# You can edit the country (R) is bg
 os.system("sed -i 's/ftp.bg/ftp.gr/' /etc/apt/sources.list");
 os.system("apt update -y");
 os.system("apt upgrade -y");
